171c40d16a14e30c3189850085963e02ca745ea2
KC Sivaramakrishnan <kc@kcsrk.info>
2018-08-28 11:27:20 +0000

    minor fix to script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # Run for the initial setup.
 ## Install opam.2.0.0
 
-opam create operf 4.02.3
+opam switch create operf 4.02.3
 opam pin add -y -k git git://github.com/kayceesrk/ocaml-perf
 opam pin add -y -k git git://github.com/OCamlPro/operf-macro#opam2
 opam install yojson re
