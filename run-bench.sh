5b27a5cd8468568d1951b993a0a8e18978213ef5
KC Sivaramakrishnan <kc@kcsrk.info>
2018-09-10 23:10:37 +0100

    Plot everything

--- run-bench.sh
+++ run-bench.sh
@@ -279,31 +279,29 @@ make_html () {
 	</ul></body></html>" >build.html
 
 	echo "<html><head><title>bench index</title></head><body>
-		<script src=\"https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js\"> </script>
-		<link rel=\"stylesheet\" type=\"text/css\" href=\"https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css\">
-		<style type=\"text/css\" class=\"init\">
+		<script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js'> </script>
+		<link rel='stylesheet' type='text/css' href='https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css'>
+		<style type='text/css' class='init'>
 
 		</style>
-		<script type=\"text/javascript\" language=\"javascript\" src=\"https://code.jquery.com/jquery-3.3.1.js\"></script>
-		<script type=\"text/javascript\" language=\"javascript\" src=\"https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js\"></script>
+		<script type='text/javascript' language='javascript' src='https://code.jquery.com/jquery-3.3.1.js'></script>
+		<script type='text/javascript' language='javascript' src='https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js'></script>
 
-		<script src=\"compare.js\"></script>
-		<script type=\"text/javascript\" class=\"init\">
+		<script src='compare.js'></script>
+		<script type='text/javascript' class='init'>
 			\$(document).ready(function() {
-				\$('#example').DataTable({ \"order\": [[ 0, \"desc\" ]] });
+				\$('#example').DataTable({ 'order': [[ 0, 'desc' ]] });
 			} );
 		</script>
-		<div id=\"container\" style=\"width: 90%;\"> <canvas id=\"chart\"></canvas> </div>
-		<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">
+		<div id='container' style='width: 90%;'> <canvas id='chart'></canvas> </div>
+		<link rel='stylesheet' type='text/css' href='style.css'>
 		</br>
-		<b>What to plot?</b> Work rate
-		<label class=\"switch\" onclick=\"plot()\">
-		<input type=\"checkbox\" id=\"speedup\">
-		<span class=\"slider round\"></span>
-		</label>
-		Running time
 		</br>
-		</br>" > index.html
+		<b> Topic </b> <select id='topic' onchange='plot()'> </select>
+		<input type='checkbox' id='normalize' onclick='plot()' checked> <b> Normalize </b>
+    </br>
+    </br>
+" > index.html
 	python $REPODIR/build_index.py >> index.html
 	echo "</body></html>" >> index.html
 }
