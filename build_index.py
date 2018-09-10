2eb472f64a3559859e151a4628ff94f190edb210
KC Sivaramakrishnan <kc@kcsrk.info>
2018-09-10 15:34:29 +0100

    Rebuild index

--- build_index.py
+++ build_index.py
@@ -4,45 +4,51 @@ import subprocess
 import os.path
 from sets import Set
 
-compilers = Set()
-s = subprocess.check_output(["find . -name '*.hash' -printf '%Ts\t%p\n' | sort -nr | cut -f2"], shell=True)
+benches = []
+compilers_set = Set()
+s = subprocess.check_output(["find . -name 'summary.csv' -printf '%Ts\t%p\n' | sort -nr | cut -f2"], shell=True)
 for line in s.split('\n'):
-	if (line == ""):
-		continue
-	compilers.add(os.path.basename(line).replace(".hash",""))
+    if line == "":
+        continue
+    with open(line) as f:
+        line2 = f.readline()
+        for c in line2.split(','):
+            if (c == "time_real" or c == "" or c == "\n"):
+                continue
+            compilers_set.add(c)
+            benches.append((os.path.dirname(line).replace("./",""), c))
 
-compilers_list = sorted(list(compilers))
+compilers_list = sorted(list(compilers_set))
 
 print '<table id="example" class="display" style="width:90%"><thead><tr>'
 print '  <th>Date</th>'
 for c in compilers_list:
-	print '  <th>' + c + '</th>'
+    print '  <th>' + c + '</th>'
 print '</tr></thead><tbody>'
 
 cur_dir = ""
 data = {}
 
-for line in s.split('\n'):
-	if (line == ""):
-		continue
-	dir = os.path.dirname(line).replace("./","")
+for b in benches:
+    dir,compiler = b
 
-	if (dir != cur_dir):
-		if (len(data) != 0):
-			print '  <tr>'
-			print '    <td>' + cur_dir + '</td>'
-			for c in compilers_list:
-				v = data.get(c)
-				if (v != None):
-					key,text = v
-					print '    <td><input type="checkbox" name="benchrun" onclick="plot()" value="' + key + '" hash="' + text +'">' + text + '</input></td>'
-				else:
-					print '    <td></td>'
-			print '  </tr>'
-			data = {}
-		cur_dir = dir
+    if (dir != cur_dir):
+        if (len(data) != 0):
+            print '  <tr>'
+            print '    <td>' + cur_dir + '</td>'
+            for c in compilers_list:
+                v = data.get(c)
+                if (v != None):
+                    key,text = v
+                    print '    <td><input type="checkbox" name="benchrun" onclick="plot()" value="' + key + '" hash="' + text +'">' + text + '</input></td>'
+                else:
+                    print '    <td></td>'
+            print '  </tr>'
+            data = {}
+        cur_dir = dir
 
-	compiler = os.path.basename(line).replace(".hash","")
-	data[compiler] = (line.replace(".hash","").replace("./",""), open(line, "r").read().replace("\n",""))
+    hash_file = "./" + dir + "/" + compiler.replace("+bench","") + ".hash"
+    if (os.path.exists(hash_file)):
+        data[compiler] = (dir + "/" + compiler, open(hash_file, "r").read().replace("\n",""))
 
 print '</table>'
