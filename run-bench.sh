c7a3a5c5a8fc47de8d2779d9f1a975b4fe2554cf
KC Sivaramakrishnan <kc@kcsrk.info>
2019-02-07 12:17:24 +0530

    Remove tabs

--- run-bench.sh
+++ run-bench.sh
@@ -274,36 +274,36 @@ cp $REPODIR/compare.js $REPODIR/style.css $BASELOGDIR
 cd $BASELOGDIR
 
 make_html () {
-	echo "<html><head><title>bench index</title></head><body><ul>
-		$(ls -dt 201* latest | sed 's%\(.*\)%<li><a href="\1/build.html">\1</a></li>%')
-	</ul></body></html>" >build.html
+  echo "<html><head><title>bench index</title></head><body><ul>
+    $(ls -dt 201* latest | sed 's%\(.*\)%<li><a href="\1/build.html">\1</a></li>%')
+  </ul></body></html>" >build.html
 
-	echo "<html><head><title>bench index</title></head><body>
-		<script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js'> </script>
-		<link rel='stylesheet' type='text/css' href='https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css'>
-		<style type='text/css' class='init'>
+  echo "<html><head><title>bench index</title></head><body>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js'> </script>
+    <link rel='stylesheet' type='text/css' href='https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css'>
+    <style type='text/css' class='init'>
 
-		</style>
-		<script type='text/javascript' language='javascript' src='https://code.jquery.com/jquery-3.3.1.js'></script>
-		<script type='text/javascript' language='javascript' src='https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js'></script>
+    </style>
+    <script type='text/javascript' language='javascript' src='https://code.jquery.com/jquery-3.3.1.js'></script>
+    <script type='text/javascript' language='javascript' src='https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js'></script>
 
-		<script src='compare.js'></script>
-		<script type='text/javascript' class='init'>
-			\$(document).ready(function() {
-				\$('#example').DataTable({ 'order': [[ 0, 'desc' ]] });
-			} );
-		</script>
-		<div id='container' style='width: 90%;'> <canvas id='chart'></canvas> </div>
-		<link rel='stylesheet' type='text/css' href='style.css'>
-		</br>
-		</br>
-		<b> Topic </b> <select id='topic' onchange='plot()'> </select>
-		<input type='checkbox' id='normalize' onclick='plot()' checked> <b> Normalize </b>
+    <script src='compare.js'></script>
+    <script type='text/javascript' class='init'>
+      \$(document).ready(function() {
+        \$('#example').DataTable({ 'order': [[ 0, 'desc' ]] });
+      } );
+    </script>
+    <div id='container' style='width: 90%;'> <canvas id='chart'></canvas> </div>
+    <link rel='stylesheet' type='text/css' href='style.css'>
+    </br>
+    </br>
+    <b> Topic </b> <select id='topic' onchange='plot()'> </select>
+    <input type='checkbox' id='normalize' onclick='plot()' checked> <b> Normalize </b>
     </br>
     </br>
 " > index.html
-	python3 $REPODIR/build_index.py >> index.html
-	echo "</body></html>" >> index.html
+  python3 $REPODIR/build_index.py >> index.html
+  echo "</body></html>" >> index.html
 }
 
 make_html
